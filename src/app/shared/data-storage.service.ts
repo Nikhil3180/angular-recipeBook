1a204c7ba4068565429d566bf0d30f9ebf309d85
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-05-12 23:39:50 +0530

    Data stored in firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ReceipeService } from '../recipes/receipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeService: ReceipeService) {}
+
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-b9aee.firebaseio.com/recipes.json', recipes)
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
 }
