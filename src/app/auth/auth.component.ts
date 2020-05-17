a7470a4d0ed6b669ada1bfba38dcee1f1c268da8
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-05-17 11:58:16 +0530

    Sign up functionality works

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,14 +10,26 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
 
   isLoginMode = true;
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 onSwitchMode() {
   this.isLoginMode = !this.isLoginMode;
 }
-onSubmit(form : NgForm){
+onSubmit(form: NgForm) {
+  if (!form.valid) {
+    return;
+  }
+  const email = form.value.email;
+  const password = form.value.password;
+  if (this.isLoginMode) {} else {
+  this.authService.signup(email, password).subscribe(data => {
+    console.log(data);
+  }, error  => {
+    console.log(error);
+  });
+}
   console.log(form);
   form.reset();
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
 
     signup(email: string, password: string) {
       // tslint:disable-next-line: max-line-length
-      return  this.http.post<AuthResponseData>('https:;// identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBW6yODwLVEIytc4dWMeuQ7i25caIasS2M',
+      return  this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBW6yODwLVEIytc4dWMeuQ7i25caIasS2M',
         {
             email: email,
             password: password,
