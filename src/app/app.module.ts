7d375acb06e4f85c05e33d401f01afd87a008657
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-05-29 22:36:59 +0530

    Reducer implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {HeaderComponent} from './header/header.component';
 import { AppComponent } from './app.component';
@@ -8,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { LoggingService } from './logging.service';
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingList : shoppingListReducer})
   ],
   providers: [LoggingService],
   bootstrap: [AppComponent]
