d9089fb64cad2ddf85721cca568329de685e2b1f
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-06-04 23:40:26 +0530

    Auth is now implemented through store everywhere in the application

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpParams } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { take, exhaust, exhaustMap } from 'rxjs/operators';
+import { take, exhaust, exhaustMap, map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
 
 
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-       return this.authService.users.pipe(take(1), exhaustMap(user => {
-           if(!user){
+       // return this.authService.users
+       return this.store.select('auth').pipe(take(1),
+       map(authState =>authState.user),
+        exhaustMap(user => {
+           if (!user) {
                return next.handle(req);
            }
         const modifiedReq = req.clone({params: new HttpParams().set('auth', user.token)});
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component , EventEmitter, Output, OnInit, OnDestroy} from '@angular/cor
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reducer';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -11,10 +14,12 @@ export class HeaderComponent  implements OnInit, OnDestroy {
 
     userData: Subscription;
     isAuthenticate = false;
-    constructor(private dataService: DataStorageService, private authService: AuthService) {}
+    constructor(private dataService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
 ngOnInit() {
-   this.userData =  this.authService.users.subscribe( data => {
+   this.userData =  this.store.select('auth')
+   .pipe(map(authState => authState.user))
+   .subscribe( data => {
        this.isAuthenticate = !data ? false : true;
    });
 }
